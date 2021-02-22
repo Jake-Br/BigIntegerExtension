d4c825370f81d146fba892775c259175d320ec28
bmuthuswamy-bhcc <b.muthuswamy@bhcc.edu>
2021-02-22 11:54:30 -0500

    Initial push of repo
    This push is to make sure repo is correctly added to bmuthuswamy-bhcc
    github.

---
+++ README.md
@@ -0,0 +1,10 @@
+This project illustrates extending BigInteger to include -infinity, infinity and NaN.
+It is for illustrative purposes, we don't claim to implement an all encompassing 
+arbitrary precision integer arithmetic engine.
+
+We use .NET 5.0 BigInteger struct, so you need the .NET 5.0 SDK.  This project has been
+successfully built on Linux using the dotnet binary installed MANUALLY and Visual Studio Code.  
+It has also been successfully built using Visual Studio 2019 Community Edition with .NET 5.0 SDK. 
+
+Only the files in src/ are cross-platform compatible.  You should use the src/ files as a starting point
+to create a new project in your build OS and IDE of choice.
---
+++ src/BigIntegerExtension.cs
@@ -0,0 +1,45 @@
+﻿using System;
+
+
+/* In lecture 4, we had a very illuminating discussion on number representation
+ * in binary:  unsigned, sign-magnitude, 1's complement and 2's complement.
+ * We realized that atomic types (int, long) have finite precision.
+ * Modern languages (like C#) support arbitrary precision arithmetic, through 
+ * the use of lazy evaluation.
+ * 
+ * Purpose of this design:  Implement arbitrary precision INTEGER arithmetic.  
+ * That is, provide support for the set of integers:
+ * 
+ * Z = {...,-3,-2,-1,0,1,2,3,...} U (-infinity,infinity) U NaN
+ * 
+ * So, our program will be able to do this:
+ * infinity + 3 = infinity
+ * infinity + infinity = infinity
+ * infinity - infinity = undefined
+ * 1/0 = infinity (think about 1/x graph, as x->0)
+ * -1/0 = -infinity
+ * 0/0 = NaN (as represented in computing systems)
+ * 
+ * But first, we NEED to pick our data structure!  Remember:  
+ * show me your data and I will show the program.
+ * Data structures we know are:
+ * List,
+ * Stack,
+ * Queue,
+ * Linked List,
+ * Dictionary,
+ * HashSet
+ * 
+ * To bound our design even further, we will utilize the already
+ * existing BigInteger struct in C# .NET framework
+ * 
+ */
+
+namespace BigIntegerExtension {
+    class BigIntegerExtension {
+        static void Main(string[] args) {
+            Console.WriteLine("Arbitrary Precision Integer Arithmetic with " +
+            	"Support for (-infinity,infinity) and NaN");
+        }
+    }
+}
